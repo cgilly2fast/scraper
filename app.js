a1c9581cafbd09b4532df0ee2ebd148d277c6c90
Ahmed Yasser <ahmedreo4@gmail.com>
2020-01-08 10:57:43 +0200

    m

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ const fs = require('fs')
 const app = express()
 const port = 3000
 
-
+const { google } = require('googleapis');
 const cheerio = require("cheerio");
 const axios = require("axios");
 const siteUrl = "https://www.hwyoneprop.com/highway-one-vacation-rentals/27-calle-del-pradero";
 
+const calendar = google.calendar({
+  version: 'v3',
+  auth: 'AIzaSyCwou7ggcvV5T6Aa8F9RYRG6DGD2ICh3io'
+})
+
+
 const availability = [];
 
-app.get('/', (req, res) => {
-  axios.get(siteUrl).then((response) => {
-    const $ = cheerio.load(response.data)
+app.get('/', (request, response) => {
+  axios.get(siteUrl).then((res) => {
+    const $ = cheerio.load(res.data)
 
     $('table.rc-calendar.rcav-month').each((index, element) => {
       let date = $(element).find('caption.rc-calendar-month.rcjs-page-caption').text()
@@ -25,8 +31,8 @@ app.get('/', (req, res) => {
         else if ($(element).hasClass('av-IN')) {
           days.push({ day: $(element).text(), type: 'checked-in' })
         }
-        else if ($(element).hasClass('av-O')) {
-          days.push({ day: $(element).text(), type: 'stayed' })
+        else if ($(element).hasClass('av-X')) {
+          days.push({ day: $(element).text(), type: 'empty' })
         }
 
       })
@@ -37,7 +43,18 @@ app.get('/', (req, res) => {
 
     fs.writeFileSync('./output.json', jsonString, 'utf-8');
 
-    res.send('done')
+    // calendar.events.list({
+    //   calendarId: 'ahmedreo4@gmail.com',
+    // }).then(res => {
+    //   const events = res.data.items;
+    //   if (events.length) {
+    //     console.log('Upcoming 10 events:');
+    //   } else {
+    //     console.log('No upcoming events found.');
+    //   }
+    // })
+
+    response.send('done')
 
   })
 })
